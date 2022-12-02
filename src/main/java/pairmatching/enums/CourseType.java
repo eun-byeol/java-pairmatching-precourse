8a7e3d7ff53e88fc2e0c787972039ded1fc17a67
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-02 15:57:28 +0900

    feat(CourseType): course 타입 enum 클래스 정의

--- src/main/java/pairmatching/enums/CourseType.java
+++ src/main/java/pairmatching/enums/CourseType.java
@@ -1,12 +1,23 @@
-//package pairmatching.enums;
-//
-//public enum Course {
-//    BACKEND("백엔드"),
-//    FRONTEND("프론트엔드");
-//
-//    private String name;
-//
-//    Course(String name) {
-//        this.name = name;
-//    }
-//}
+package pairmatching.enums;
+
+import java.util.Arrays;
+
+import static pairmatching.utils.ErrorMessages.*;
+
+public enum CourseType {
+    BACKEND("백엔드"),
+    FRONTEND("프론트엔드");
+
+    private String name;
+
+    CourseType(String name) {
+        this.name = name;
+    }
+
+    public static CourseType selectCourseByName(String courseName) {
+        return Arrays.stream(CourseType.values())
+                .filter(course -> course.name.equals(courseName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(COURSE_NOT_OPTIONS));
+    }
+}
