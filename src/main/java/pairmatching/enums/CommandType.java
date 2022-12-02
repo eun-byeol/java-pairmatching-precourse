8d4ca94b834f5cce51903767094a5524e3a26c5d
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-02 15:59:32 +0900

    refactor(CommandType): 클래스 이름 변경

--- src/main/java/pairmatching/enums/CommandType.java
+++ src/main/java/pairmatching/enums/CommandType.java
@@ -3,7 +3,7 @@ package pairmatching.enums;
 import java.util.Arrays;
 import static pairmatching.utils.ErrorMessages.*;
 
-public enum Command {
+public enum CommandType {
     PAIR_MATCHING("1", "페어 매칭"),
     PAIR_LOOKUP("2", "페어 조회"),
     PAIR_RESET("3", "페어 초기화"),
@@ -12,16 +12,16 @@ public enum Command {
     private String codeName;
     private String subject;
 
-    Command(String codeName, String subject) {
+    CommandType(String codeName, String subject) {
         this.codeName = codeName;
         this.subject = subject;
     }
 
-    public static Command selectCommandByCodeName(String codeName) {
-        return Arrays.stream(Command.values())
+    public static CommandType selectCommandByCodeName(String codeName) {
+        return Arrays.stream(CommandType.values())
                 .filter(command -> command.codeName.equals(codeName))
                 .findAny()
-                .orElseThrow(() -> new IllegalArgumentException(GAME_COMMAND_NOT_OPTIONS));
+                .orElseThrow(() -> new IllegalArgumentException(COMMAND_NOT_OPTIONS));
     }
 
     public String getCodeName() {
