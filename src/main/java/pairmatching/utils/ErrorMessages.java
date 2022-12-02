47122753a14bd392a81855fbbfdd3e964853069a
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-02 15:54:28 +0900

    feat(ErrorMessages): level 입력 오류 메시지 정의

--- src/main/java/pairmatching/utils/ErrorMessages.java
+++ src/main/java/pairmatching/utils/ErrorMessages.java
@@ -2,5 +2,7 @@ package pairmatching.utils;
 
 public class ErrorMessages {
     private static final String ERROR_TAG = "[ERROR] ";
-    public static final String GAME_COMMAND_NOT_OPTIONS = ERROR_TAG + "1~3 혹은 Q 중 하나의 문자여야 합니다.";
+    public static final String COMMAND_NOT_OPTIONS = ERROR_TAG + "1~3 혹은 Q 중 하나의 문자여야 합니다.";
+    public static final String COURSE_NOT_OPTIONS = ERROR_TAG + "백엔드, 프론트엔드 중 하나의 단어여야 합니다.";
+    public static final String LEVEL_NOT_OPTIONS = ERROR_TAG + "레벨1~5 중 하나의 정확한 단어여야 합니다.";
 }
