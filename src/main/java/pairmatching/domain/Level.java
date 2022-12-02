fd7aed2d665324cf9aa481e0ec4ac31b94c51779
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-02 15:55:01 +0900

    feat(Level): Level 입력 유효성 검사 구현

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,4 +1,15 @@
 package pairmatching.domain;
 
+import pairmatching.enums.LevelType;
+
 public class Level {
+    private final String levelName;
+    public Level(String levelName) {
+        validateLevelType(levelName);
+        this.levelName = levelName;
+    }
+
+    private void validateLevelType(String name) throws IllegalArgumentException{
+        LevelType.selectLevelTypeByName(name);
+    }
 }
