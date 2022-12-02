493a3e9dd12c2eceecffcebea9404acc94c82594
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-02 15:28:19 +0900

    feat(ErrorMessages): command 오입력시 오류 메시지 정의

---
+++ src/main/java/pairmatching/utils/ErrorMessages.java
@@ -0,0 +1,6 @@
+package pairmatching.utils;
+
+public class ErrorMessages {
+    private static final String ERROR_TAG = "[ERROR] ";
+    public static final String GAME_COMMAND_NOT_OPTIONS = ERROR_TAG + "1~3 혹은 Q 중 하나의 문자여야 합니다.";
+}
