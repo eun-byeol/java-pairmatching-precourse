f62c6a6d2b213798b9c40d78149af2fa15c0f0bd
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 15:49:12 +0900

    feat(CourseType): getPath 메서드 추가

--- src/main/java/pairmatching/enums/CourseType.java
+++ src/main/java/pairmatching/enums/CourseType.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 import static pairmatching.utils.ErrorMessages.*;
 
 public enum CourseType {
-    BACKEND("백엔드"),
-    FRONTEND("프론트엔드");
+    BACKEND("백엔드", "/Users/jo-eunbyeol/wooteco/java-pairmatching-precourse/src/main/resources/backend-crew.md"),
+    FRONTEND("프론트엔드", "/Users/jo-eunbyeol/wooteco/java-pairmatching-precourse/src/main/resources/frontend-crew.md");
 
     private String name;
+    private String path;
 
-    CourseType(String name) {
+    CourseType(String name, String path) {
         this.name = name;
+        this.path = path;
     }
 
     public static CourseType selectCourseByName(String courseName) {
@@ -19,5 +21,13 @@ public enum CourseType {
                 .filter(course -> course.name.equals(courseName))
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(COURSE_NOT_OPTIONS));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
     }
 }
