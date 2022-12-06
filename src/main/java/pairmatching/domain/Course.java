c25425b09867ff633c788f91a33da664eb799c64
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 15:23:22 +0900

    feat(Course): 멤버 변수 추가

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,20 +1,34 @@
 package pairmatching.domain;
 
 import pairmatching.enums.CourseType;
+import pairmatching.enums.LevelType;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Course {
+    private HashMap<String, Level> levels;
     private final List<String> crewNames;
-    private final String courseName;
+    private final CourseType courseType;
 
-    public Course(List<String> crewNames, String courseName) {
+    public Course(List<String> crewNames, CourseType courseType) {
+        this.levels = new HashMap<>();
+        setUpLevels();
         this.crewNames = crewNames;
-        validateCourseType(courseName);
-        this.courseName = courseName;
+        this.courseType = courseType;
     }
 
-    private void validateCourseType(String courseName) throws IllegalArgumentException{
-        CourseType.selectCourseByName(courseName);
+    private void setUpLevels() {
+        for (LevelType leveltype : LevelType.values()) {
+            this.levels.put(leveltype.getName(), new Level(leveltype));
+        }
     }
+
+    public List<String> getCrewNames() {
+        return crewNames;
+    }
+
+    //    private void validateCourseType(CourseType courseName) throws IllegalArgumentException{
+//        CourseType.selectCourseByName(courseName);
+//    }
 }
