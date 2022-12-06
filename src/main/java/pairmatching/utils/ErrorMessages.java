b8c20d9e57d79c32ffed0677bdff1cb6321aadc1
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 21:25:05 +0900

    feat(ErrorMessages): 매칭 실패시 오류 메시지 정의

--- src/main/java/pairmatching/utils/ErrorMessages.java
+++ src/main/java/pairmatching/utils/ErrorMessages.java
@@ -5,4 +5,6 @@ public class ErrorMessages {
     public static final String COMMAND_NOT_OPTIONS = ERROR_TAG + "1~3 혹은 Q 중 하나의 문자여야 합니다.";
     public static final String COURSE_NOT_OPTIONS = ERROR_TAG + "백엔드, 프론트엔드 중 하나의 단어여야 합니다.";
     public static final String LEVEL_NOT_OPTIONS = ERROR_TAG + "레벨1~5 중 하나의 정확한 단어여야 합니다.";
+    public static final String MISSION_NOT_OPTIONS = ERROR_TAG + "유효하지 않은 미션 이름입니다.";
+    public static final String MATCHING_FAIL = ERROR_TAG + "3회 이상 매칭이 되지 않거나 매칭을 할 수 있는 경우의 수가 없습니다.";
 }
