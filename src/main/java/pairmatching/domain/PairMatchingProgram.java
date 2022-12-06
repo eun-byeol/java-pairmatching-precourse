09bc6672c30cf4e79fa47544dca97c1609cbcae6
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 21:19:42 +0900

    feat(PairMatchingProgram): 페어매칭 기능 구현

--- src/main/java/pairmatching/domain/PairMatchingProgram.java
+++ src/main/java/pairmatching/domain/PairMatchingProgram.java
@@ -4,10 +4,16 @@ import pairmatching.enums.CourseType;
 import pairmatching.utils.ReadFile;
 
 import java.io.IOException;
+import java.util.List;
+
+import static pairmatching.utils.ErrorMessages.MATCHING_FAIL;
 
 public class PairMatchingProgram {
     private Course backendCourse;
     private Course frontendCourse;
+
+    private PairMatchingMaker pairMatchingMaker;
+    private PairMatchingChecker pairMatchingChecker;
 
     public PairMatchingProgram() {
         CourseType backend = CourseType.valueOf("BACKEND");
@@ -24,5 +30,32 @@ public class PairMatchingProgram {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        this.pairMatchingMaker = new PairMatchingMaker();
+        this.pairMatchingChecker = new PairMatchingChecker();
+    }
+
+    public List<List<String>> runPairMatching(String course, String level, String mission) throws Exception {
+        int count = 0;
+        Course currentCourse = selectCourseType(course);
+        Level currentLevel = currentCourse.getLevels().get(level);
+        Mission currentMission = currentLevel.getMissions().get(mission);
+        List<String> crewNames = currentCourse.getCrewNames();
+
+        while (count < 3) {
+            List<List<String>> result = pairMatchingMaker.returnPairMatching(crewNames);
+            if (!pairMatchingChecker.checkDuplication(currentLevel, result)) {
+                currentMission.setMatchingResult(result);
+                return result;
+            }
+            count++;
+        }
+        throw new Exception(MATCHING_FAIL);
+    }
+
+    private Course selectCourseType(String course) {
+        if (CourseType.valueOf("BACKEND").getName().equals(course)) {
+            return this.backendCourse;
+        }
+        return this.frontendCourse;
     }
 }
