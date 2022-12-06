7e316c3c96c4cf74a1d7d8f2a832a72f1ea4f936
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 15:45:12 +0900

    feat(Mission): 멤버 변수 정의, 생성자 추가

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,4 +1,18 @@
 package pairmatching.domain;
 
+import pairmatching.enums.MissionType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mission {
+    private final MissionType missionType;
+    private boolean isMatching;
+    private List<List<String>> matchingResult;
+
+    public Mission(MissionType missionType) {
+        this.missionType = missionType;
+        this.isMatching = false;
+        this.matchingResult = new ArrayList<>();
+    }
 }
