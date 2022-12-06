724b4135dad5ebbb672146738b6e06e4a106ca75
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 15:34:25 +0900

    feat(Level): 멤버 변수 추가

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,15 +1,30 @@
 package pairmatching.domain;
 
 import pairmatching.enums.LevelType;
+import pairmatching.enums.MissionType;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class Level {
-    private final String levelName;
-    public Level(String levelName) {
-        validateLevelType(levelName);
-        this.levelName = levelName;
+    private final LevelType levelType;
+    private HashMap<String, Mission> missions;
+
+    public Level(LevelType levelType) {
+        this.levelType = levelType;
+        this.missions = new HashMap<>();
+        setUpMissions();
+//        validateLevelType(levelType);
     }
 
-    private void validateLevelType(String name) throws IllegalArgumentException{
-        LevelType.selectLevelTypeByName(name);
+    private void setUpMissions() {
+        List<MissionType> missionTypes = this.levelType.getMissions();
+        for (MissionType missionType: missionTypes) {
+            this.missions.put(missionType.getName(), new Mission(missionType));
+        }
     }
+
+//    private void validateLevelType(String name) throws IllegalArgumentException{
+//        LevelType.selectLevelTypeByName(name);
+//    }
 }
