690c3b6d7f92731d5f5ebb2e9d28cde4462dec3f
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-06 21:22:38 +0900

    feat(Controller): 페어 매칭 호출부 구현

--- src/main/java/pairmatching/controller/Controller.java
+++ src/main/java/pairmatching/controller/Controller.java
@@ -3,18 +3,18 @@ package pairmatching.controller;
 import pairmatching.domain.PairMatchingProgram;
 import pairmatching.domain.UserCommand;
 
-import static pairmatching.view.InputView.readCourseAndLevelAndMission;
-import static pairmatching.view.InputView.readProgramCommand;
+import java.util.ArrayList;
+import java.util.List;
+
+import static pairmatching.view.InputView.*;
+import static pairmatching.view.OutputView.*;
 
 public class Controller {
+    private boolean isStop = false;
     public void run() {
         PairMatchingProgram pairMatchingProgram = new PairMatchingProgram();
-
-        UserCommand command = inputUserCommand();
-
-        String userInput = readCourseAndLevelAndMission();
-        String[] inputs = userInput.split(", ");
-        for (String input : inputs) {
+        while (!isStop) {
+            ExecuteCommand(inputUserCommand(), pairMatchingProgram);
         }
     }
 
@@ -25,6 +25,38 @@ public class Controller {
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private void ExecuteCommand(UserCommand command, PairMatchingProgram pairMatchingProgram) {
+        if (command.getCommand().equals("1")) {
+            System.out.println("1");
+            String userInput = readCourseAndLevelAndMission();
+            String[] inputs = userInput.split(",");
+            List<String> inputList = new ArrayList<>();
+            for (String input : inputs) {
+                inputList.add(input.trim());
+            }
+            try {
+                List<List<String>> result = pairMatchingProgram.runPairMatching(
+                        inputList.get(0),
+                        inputList.get(1),
+                        inputList.get(2)
+                );
+                printPairMatchingResult(result);
+                return;
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        if (command.getCommand().equals("2")) {
+
+        }
+        if (command.getCommand().equals("2")) {
+
+        }
+        if (command.getCommand().equals("Q")) {
+            this.isStop = true;
         }
     }
 }
